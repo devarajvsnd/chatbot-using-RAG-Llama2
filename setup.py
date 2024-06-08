d310c3c7459409e9cc0cc6d84ed1257ff2a5a52d
devarajvsnd <devarajvsnd@gmail.com>
2024-06-08 13:00:23 +0530

    Code updated

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Chatbot',
+    version= '0.0.0',
+    author= 'Devaraj Vishwakarma',
+    author_email= 'devarajvsnd@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
